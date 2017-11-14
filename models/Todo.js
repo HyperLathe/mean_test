e6b2a16599bc148a1e05126d48dafdc3ef4c4026
Richard Young <info@hyperlathe.com>
2017-11-14 12:39:43 +0000

    First commit

---
+++ models/Todo.js
@@ -0,0 +1,7 @@
+var mongoose = require('mongoose');
+var TodoSchema = new mongoose.Schema({
+  name: String,
+  completed: Boolean,
+  note: String,
+});
+module.exports = mongoose.model('Todo', TodoSchema);
